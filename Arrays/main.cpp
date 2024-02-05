2caa50faff7f53fe2457396df32754e299676b99
PC <PC@MSI-PC97>
2024-02-05 21:37:35 +0300

    introduction toArrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 void main() {
 	setlocale(LC_ALL, "");
-
+	cout << "Heloo Arrays" << endl;
+	const int SIZE = 5;
+	int arr[SIZE] ;
+	cout << int() << endl;
+	
+	for (int i = 0; i < SIZE; i++) {
+		cout << "¬ведите значение : \n";
+		cin >> arr[i];
+	}
+	for (int i = 0; i < SIZE; i++) {
+		cout << arr[i] << "\t";
+		
+	}
+	cout << endl;
 }
